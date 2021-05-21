6c0554fc7b5e626adcc2a9ee94cf075ade77c169
Ikem Nomso <ikemnomso@gmail.com>
2021-05-21 13:14:05 +0100

    info component

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,24 +1,24 @@
 import React from 'react';
-import { info } from '../css/Info.module.css';
+import { info, resultItem } from '../css/Info.module.css';
 
 const Info = () => {
 	return (
 		<div className={info}>
-			<div>
+			<div className={resultItem}>
 				<h3>IP ADDRESS</h3>
-				<p>LOREM</p>
+				<p>192.212.174.101</p>
 			</div>
-			<div>
+			<div className={resultItem}>
 				<h3>LOCATION</h3>
-				<p>LOREM</p>
+				<p>Brooklyn, NY 10001</p>
 			</div>
-			<div>
+			<div className={resultItem}>
 				<h3>TIMEZONE</h3>
-				<p>LOREM</p>
+				<p>UTC -05:00</p>
 			</div>
-			<div>
+			<div className={resultItem}>
 				<h3>ISP</h3>
-				<p>LOREM</p>
+				<p>SpaceX Starlink</p>
 			</div>
 		</div>
 	);
